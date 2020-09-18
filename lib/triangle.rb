d3b756c1fcfd2dc3dac688140f01d3db456d13a8
Jeremy Wood <js.wood206@gmail.com>
2020-09-18 04:49:20 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one,side_two,side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+    if side_one == 0 || side_two == 0 || side_three == 0
+      raise TriangleError
+    elsif side_one.negative? || side_two.negative? || side_three.negative?
+      raise TriangleError
+    elsif side_one + side_two <= side_three || side_two + side_three <= side_one || side_one + side_three <= side_two
+      raise TriangleError
+    elsif side_one == side_two && side_two == side_three
+      :equilateral
+    elsif side_one == side_two || side_one == side_three || side_two == side_three
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
 end
