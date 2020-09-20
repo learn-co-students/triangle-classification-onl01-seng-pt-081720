afcec78ece4c517f9d7140d8f6f6871c45e44742
Hector_Jimenez <hjmontemayor@gmail.com>
2020-09-20 13:45:06 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,58 @@
 class Triangle
-  # write code here
+  attr_accessor :first, :second, :third
+
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+  end
+
+  def kind 
+    if @first == 0 || @second == 0 || @third == 0
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message_zero
+      end
+
+    elsif @first < 0 || @second < 0 || @third < 0
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message_negative
+      end
+
+    elsif @first + @second < @third || @first + @third < @second || @second + @third < @first
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message_inequality
+      end
+
+    elsif @first == @second && @second == @third
+      return :equilateral
+
+    elsif (@first != @second && @second == @third) || (@second != @third && @third == @first) || (@first != @third && @second == @first)
+      return :isosceles
+
+    elsif @first != @second && @second != @third
+      return :scalene
+
+    end
+  end
+
+  
+  class TriangleError < StandardError
+    def message_zero
+      "Triangle Error: Zero values not allowed"
+    end
+
+    def message_inequality
+      "Triangle Error: Inequality"
+    end
+
+    def message_negative
+      "Triangle Error: Negative numbers not allowed!"
+    end
+  end
 end
