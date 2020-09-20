c609442fcdf8f7e139e123a61158be96e62f958c
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-20 12:06:40 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,48 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  attr_accessor :l_one, :l_two, :l_three 
+  
+  def initialize(l_one, l_two, l_three)
+    @l_one = l_one 
+    @l_two = l_two 
+    @l_three = l_three
+    @all_sides = [@l_one, @l_two, @l_three]
+    @all_sides.sort!
+  end 
+  
+  
+  def valid_triangle
+     @l_one + @l_two > @l_three && @l_one + @l_three > @l_two && @l_two + @l_three > @l_one 
+   end
+  
+  def valid_triangle_two
+    @l_one.positive? && @l_two.positive? && @l_three.positive? 
+  end
+  
+  def kind 
+    if valid_triangle && valid_triangle_two 
+      puts true 
+    else 
+      raise TriangleError 
+    end 
+ 
+  
+    if @l_one == @l_two && @l_two == @l_three && @l_three == @l_one
+      :equilateral 
+    elsif @l_one != @l_two && @l_two != @l_three && @l_three != @l_one
+     :scalene
+    else 
+      :isosceles
+  end 
+end 
+  
+  
+  class TriangleError < StandardError 
+    def message 
+     "this triangle is invalid"
+   end 
+  
+end
+  
 end
